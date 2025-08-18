ca176ff899d620b42d63426bf0607d97d784d079
ABOUD Ibrahim <li_aboud@esi.dz>
2025-08-18 23:46:51 +0100

    added one missing token

--- step_input_prediction/tinypy_code_tracer_tokenizer.py
+++ step_input_prediction/tinypy_code_tracer_tokenizer.py
@@ -7,7 +7,7 @@ class TinypyTokenizer():
 		# defining the keywords list
 		self.keywords = sorted([
 			# ' ',
-			'# ', '&', ';', '?', '|', '@', '^', '$',
+			'# ', '&', ';', '?', '|', '@', '^', '$', '~'
 			'# input?','# output\n', '# code\n', '\n#STEP\n',
 			'\n', '\t', '\n\n',
 			'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
